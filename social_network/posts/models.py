5ca179f968e22d5a76f6857b0d469df2770d55f1
Boris <boriskaborodin2009@gmail.com>
2025-09-18 14:17:31 +0500

    my commit

--- social_network/posts/models.py
+++ social_network/posts/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    text = models.TextField()
+    image = models.ImageField(upload_to='posts/')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text[:50]
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Comment by {self.user.username} on {self.post}'
+
+class Like(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ['post', 'user']
+
+    def __str__(self):
+        return f'Like by {self.user.username} on {self.post}'
