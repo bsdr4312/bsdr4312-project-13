fb4a9c774cea2aba64189cb6af50aae1a29f2059
Boris <boriskaborodin2009@gmail.com>
2025-09-27 14:19:59 +0500

    dfswkoifcsw

---
+++ social_network/posts/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from .models import Post, Comment, Like
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.CharField(source='user.username', read_only=True)
+
+    class Meta:
+        model = Comment
+        fields = ['id', 'author', 'text', 'created_at']
+
+class PostSerializer(serializers.ModelSerializer):
+    comments = CommentSerializer(many=True, read_only=True)
+    likes_count = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Post
+        fields = ['id', 'text', 'image', 'created_at', 'comments', 'likes_count']
+
+    def get_likes_count(self, obj):
+        return obj.likes.count()
--- social_network/posts/views.py
+++ social_network/posts/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
 
-# Create your views here.
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        post_id = self.kwargs['post_id']
+        post = Post.objects.get(id=post_id)
+        serializer.save(user=self.request.user, post=post)
